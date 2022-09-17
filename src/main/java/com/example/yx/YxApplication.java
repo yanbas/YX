b49d07004289e46064ec41187abeeb3c96c00a71
Yandi Bastian <yandi.bastian@klikdokter.com>
2022-09-17 19:39:39 +0700

    init

---
+++ src/main/java/com/example/yx/YxApplication.java
@@ -0,0 +1,13 @@
+package com.example.yx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yx/YxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
